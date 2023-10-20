e14918cfc21e00d76f27cd92d13c2f1838732dae
hardik <hardik>
2023-10-20 17:50:58 +0530

    2556

--- 2556.py
+++ 2556.py
@@ -3,65 +3,31 @@ from typing import List
 
 class Solution:
     def isPossibleToCutPath(self, grid: List[List[int]]) -> bool:
-        if grid == [[1, 1]]:
+        def dfs(i, j):
+            if i == len(grid) - 1 and j == len(grid[0]) - 1:
+                return True
+            grid[i][j] = 0
+            for t in [[1, 0], [0, 1]]:
+                new_var = i + t[0]
+                new_var1 = j + t[1]
+                if not (0 <= new_var < len(grid) and 0 <= new_var1 < len(grid[0])):
+                    continue
+                if grid[new_var][new_var1] and dfs(new_var, new_var1):
+                    return True
             return False
 
-        queue = [[0, 0]]
-        vited = set()
-        count = 0
-        alenth = len(grid) - 1
-        blenth = len(grid[0]) - 1
-        while queue:
-            current = queue.pop(0)
-
-            if tuple(current) in vited:
-                continue
-
-            vited.add(tuple(current))
-
-            for i in [[0, -1], [1, 0], [0, 1], [-1, 0]]:
-                new_var = [current[0] + i[0], current[1] + i[1]]
-                if not (0 <= new_var[0] <= alenth and 0 <= new_var[1] <= blenth):
-                    continue
-                if grid[new_var[0]][new_var[1]] == 0:
-                    continue
-                if new_var == [alenth, blenth]:
-                    count += 1
-                queue.append(new_var)
-        print(count, "count")
-        return count <= 1
+        if not dfs(0, 0):
+            return True
+        grid[0][0] = 1
+        return not dfs(0, 0)
 
 
 r = Solution().isPossibleToCutPath(
     [
-        [1, 1, 1, 0, 0],
-        [1, 0, 1, 0, 0],
-        [1, 1, 1, 1, 1],
-        [0, 0, 1, 1, 1],
-        [0, 0, 1, 1, 1],
+        [1, 1, 1, 1, 1, 1],
+        [1, 0, 1, 1, 1, 1],
+        [1, 1, 1, 0, 1, 1],
+        [0, 0, 0, 1, 1, 1],
     ]
 )
 print(r)
-
-# Solution().isPossibleToCutPath(
-#     [
-#         [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
-#         [0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1],
-#     ]
-# )
-
-[
-    [1, 1, 1],
-    [1, 0, 0],
-    [1, 1, 1],
-]
