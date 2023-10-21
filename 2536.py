05095fdc9372b9bcdd0bab3bfad3fce59facc427
hardik <hardik>
2023-10-21 14:35:00 +0530

    update

--- 2536.py
+++ 2536.py
@@ -1,38 +1,59 @@
 from typing import List
 
-import bisect
-
-bisect.bisect([], 1, key=lambda i: i[0])
-
 
 class Solution:
     def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
-        one = [[i[1][0], i[0]] for i in enumerate(queries)]
-        two = [[i[1][1], i[0]] for i in enumerate(queries)]
-        three = [[i[1][2], i[0]] for i in enumerate(queries)]
-        four = [[i[1][3], i[0]] for i in enumerate(queries)]
+        arr = [[0 for _ in range(n)] for _ in range(n)]
 
-        one.sort(key=lambda t: t[0])
-        two.sort(key=lambda t: t[0])
-        three.sort(key=lambda t: t[0])
-        four.sort(key=lambda t: t[0])
+        for i in queries:
+            row1, col1, row2, col2 = i
+            for j in range(row1, row2 + 1):
+                arr[j][col1] += 1
+                if col2 + 1 < n:
+                    arr[j][col2 + 1] += -1
 
-        for q in range(n):
-            for z in range(n):
-                index = bisect.bisect_left(one, q, key=lambda l: l[0])
-                anwar = set([i[1] for i in one][index:])
+        for i in range(n):
+            for j in range(1, n):
+                arr[i][j] += arr[i][j - 1]
 
-                index = bisect.bisect_left(two, q, key=lambda l: l[0])
-                anwar = anwar & set([i[1] for i in two][:index])
-
-                index = bisect.bisect_left(three, z, key=lambda l: l[0])
-                anwar = anwar & set([i[1] for i in three][index:])
-
-                index = bisect.bisect_left(four, z, key=lambda l: l[0])
-                anwar = anwar & set([i[1] for i in four][:index])
-
-                anwar = len(anwar)
-                print(anwar)
+        return arr
 
 
 Solution().rangeAddQueries(3, [[1, 1, 2, 2], [0, 0, 1, 1]])
+
+[
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 0, 0],
+    [2, 2, 5, 5, 4, 4, 4, 2, 2, 2, 1, 0, 0],
+    [2, 2, 5, 7, 6, 6, 6, 4, 3, 3, 2, 1, 1],
+    [2, 2, 4, 6, 8, 8, 8, 4, 3, 3, 2, 1, 1],
+    [2, 2, 4, 6, 8, 8, 8, 4, 3, 3, 2, 1, 1],
+    [2, 2, 4, 6, 8, 8, 8, 4, 3, 3, 2, 1, 1],
+    [2, 2, 2, 4, 4, 4, 4, 5, 3, 3, 2, 1, 1],
+    [2, 2, 2, 3, 3, 3, 3, 3, 3, 3, 2, 1, 1],
+    [2, 2, 2, 3, 3, 3, 3, 3, 3, 5, 4, 2, 1],
+    [1, 1, 1, 2, 2, 2, 2, 2, 2, 4, 6, 3, 1],
+    [0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 3, 4, 1],
+    [0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
+]
+
+[
+    [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
+    [0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1],
+    [0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
+    [0, 1, 3, 3, 2, 2, 2, 2, 3, 3, 2, 2, 2],
+    [0, 1, 3, 3, 2, 2, 2, 2, 3, 4, 3, 3, 4],
+    [0, 1, 3, 3, 2, 2, 2, 2, 3, 4, 3, 3, 4],
+    [0, 1, 3, 3, 2, 2, 2, 2, 3, 4, 3, 3, 4],
+    [0, 1, 3, 3, 2, 3, 3, 2, 2, 1, 0, 0, 1],
+    [0, 0, 2, 2, 2, 2, 2, 1, 1, 1, 0, 0, 1],
+    [0, 0, 2, 3, 3, 3, 3, 2, 2, 2, 1, 2, 1],
+    [0, 0, 1, 2, 2, 3, 3, 4, 4, 4, 3, 4, 3],
+    [0, 0, 1, 2, 2, 3, 4, 4, 3, 3, 2, 2, 1],
+    [0, 0, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 0],
+]
+
+
+a, b = [1, 2]
+a
+b
