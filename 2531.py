7685900fdf1f35833855345bb76bc4b3413d3d85
hardik <hardik>
2023-10-21 16:47:58 +0530

    2531

--- 2531.py
+++ 2531.py
@@ -1,40 +1,29 @@
 from typing import List
+import functools
 
 
 class Solution:
     def isThereAPath(self, grid: List[List[int]]) -> bool:
-        numberOfOne = len(grid) + len(grid[0]) - 1
-
-        if numberOfOne % 2 != 0:
+        alen = len(grid)
+        blen = len(grid[0])
+        oneCount = alen + blen - 1
+        if oneCount % 2 != 0:
             return False
-        numberOfOne = numberOfOne / 2
+        oneCount = oneCount / 2
 
-        if grid[0][0] == 0:
-            queue = [[0, 0, numberOfOne - 1, numberOfOne]]
-        else:
-            queue = [[0, 0, numberOfOne, numberOfOne - 1]]
-
-        while queue:
-            i = queue.pop(0)
-            if i[2] < 0:
-                continue
-            if i[3] < 0:
-                continue
-            if i[0] == len(grid) - 1 and i[1] == len(grid[0]) - 1:
+        # @functools.cache
+        def dfs(i, j, k):
+            if i >= alen or j >= blen:
+                return False
+            if k > oneCount:
+                return False
+            if i == alen - 1 and j == blen - 1 and k == oneCount:
                 return True
-            for j in [[0, 1], [1, 0]]:
-                new_var = i[0] + j[0]
-                new_var1 = i[1] + j[1]
-                if new_var >= len(grid):
-                    continue
-                if new_var1 >= len(grid[0]):
-                    continue
-                if grid[new_var][new_var1] == 0:
-                    queue.append([new_var, new_var1, i[2] - 1, i[3]])
-                else:
-                    queue.append([new_var, new_var1, i[2], i[3] - 1])
-        return False
+            new_var = k + grid[i][j]
+            return dfs(i + 1, j, new_var) or dfs(i, j + 1, new_var)
+
+        return dfs(0, 0, 0)
 
 
-r = Solution().isThereAPath([[1, 1, 0], [0, 0, 1], [1, 0, 0]])
+r = Solution().isThereAPath([[0, 1, 0, 0], [0, 1, 0, 0], [1, 0, 1, 0]])
 print(r)
