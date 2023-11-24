60fdded56b77bb619ebbc759d1c605956a67bf64
hardik <hardik>
2023-11-24 15:53:32 +0530

    update

---
+++ 2271.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def maximumWhiteTiles(self, tiles: List[List[int]], carpetLen: int) -> int:
+        print(
+            tiles,
+        )
+        pass
