e642396727bd242ac734386251ba188e7846ea75
hardik <hardik>
2023-11-24 15:46:27 +0530

    update

---
+++ 2280.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+class Solution:
+    def minimumLines(self, stockPrices: List[List[int]]) -> int:
+        pass
--- 2284.py
+++ 2284.py
@@ -1,16 +1,29 @@
 from typing import List
-import functools
+import collections
 
 
 class Solution:
     def largestWordCount(self, messages: List[str], senders: List[str]) -> str:
-        count = {}
+        count = collections.defaultdict(int)
 
         for i in range(len(messages)):
             c = messages[i].count(" ") + 1
-            if senders[i] in count:
-                count[senders[i]] += c
-            else:
-                count[senders[i]] = c
+            count[senders[i]] += c
+        maxR = 0
+        r = []
+        for i in count:
+            if count[i] > maxR:
+                r = [i]
+                maxR = count[i]
 
-        return messages
+            if count[i] == maxR:
+                r.append(i)
+        r.sort()
+        return r[-1]
+
+
+r = Solution().largestWordCount(
+    ["How is leetcode for everyone", "Leetcode is useful for practice"],
+    ["Bob", "Charlie"],
+)
+print(r)
