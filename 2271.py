d2da4341b88854a89cee7914905963c455289139
hardik <hardik>
2023-11-25 09:51:32 +0530

    update

--- 2271.py
+++ 2271.py
@@ -3,7 +3,10 @@ from typing import List
 
 class Solution:
     def maximumWhiteTiles(self, tiles: List[List[int]], carpetLen: int) -> int:
-        print(
-            tiles,
-        )
-        pass
+        print(tiles)
+        return 0
+
+
+t = Solution().maximumWhiteTiles([[1, 5], [10, 11], [12, 18], [20, 25], [30, 32]], 10)
+
+print(t)
