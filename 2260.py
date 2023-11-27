65b524c23176a6155b689eed2110eced2c25bdfc
hardik <hardik>
2023-11-27 18:04:59 +0530

    update

---
+++ 2260.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def minimumCardPickup(self, cards: List[int]) -> int:
+        r = float("inf")
+        m = dict()
+
+        for i, v in enumerate(cards):
+            if v in m:
+                r = min(r, i - m[v])
+            m[v] = i
+        return r + 1 if r != float("inf") else -1
+
+
+r = Solution().minimumCardPickup([2, 0, 2, 0, 2, 2])
+print(r)
--- 2271.py
+++ 2271.py
@@ -1,12 +1,30 @@
+from bisect import bisect_right
 from typing import List
 
 
 class Solution:
     def maximumWhiteTiles(self, tiles: List[List[int]], carpetLen: int) -> int:
-        print(tiles)
-        return 0
+        tiles.sort(key=lambda x: x[0])
 
+        startPos = [tiles[i][0] for i in range(len(tiles))]
 
-t = Solution().maximumWhiteTiles([[1, 5], [10, 11], [12, 18], [20, 25], [30, 32]], 10)
+        preSum = [0] * (len(tiles) + 1)
+        for i in range(1, len(tiles) + 1):
+            preSum[i] = preSum[i - 1] + (tiles[i - 1][1] - tiles[i - 1][0] + 1)
 
-print(t)
+        res = 0
+        for i in range(len(tiles)):
+            s, e = tiles[i]
+
+            if e >= s + carpetLen - 1:
+                return carpetLen
+
+            endIdx = bisect_right(startPos, s + carpetLen - 1) - 1
+
+            compensate = 0
+            if tiles[endIdx][1] > s + carpetLen - 1:
+                compensate = tiles[endIdx][1] - s - carpetLen + 1
+
+            res = max(res, preSum[endIdx + 1] - preSum[i] - compensate)
+
+        return res
