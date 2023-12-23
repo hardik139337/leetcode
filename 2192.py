fc013142dcb187491c56ddfe835907a471f2d0df
hardik <hardik>
2023-12-23 11:16:26 +0530

    update

---
+++ 2192.py
@@ -0,0 +1,7 @@
+from typing import List
+
+
+class Solution:
+    def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
+        print(n, edges)
+        return []
---
+++ left2187.py
@@ -0,0 +1,3 @@
+class Solution:
+    def minimumTime(self, time: list[int], totalTrips: int) -> int:
+        pass
