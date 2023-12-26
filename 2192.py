03dbea150da457c50252af7cc6691c5e9b165e30
hardik <hardik>
2023-12-26 17:44:09 +0530

    Add getAncestors method to Solution class

--- 2192.py
+++ 2192.py
@@ -1,7 +1,28 @@
 from typing import List
+import functools, collections
 
 
 class Solution:
     def getAncestors(self, n: int, edges: List[List[int]]) -> List[List[int]]:
-        print(n, edges)
-        return []
+        graph = collections.defaultdict(list)
+        for i in edges:
+            graph[i[1]].append(i[0])
+
+        @functools.cache
+        def dfs(i):
+            r = set()
+            for j in graph[i]:
+                r.add(j)
+                for t in dfs(j):
+                    r.add(t)
+
+            return r
+
+        return [sorted(list(dfs(i))) for i in range(n)]
+
+
+a = Solution().getAncestors(
+    8, [[0, 3], [0, 4], [1, 3], [2, 4], [2, 7], [3, 5], [3, 6], [3, 7], [4, 6]]
+)
+
+print(a)
