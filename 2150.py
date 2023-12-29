21981e8e1376d2699e6e24993e35f3a298ae02d9
hardik <hardik>
2023-12-29 11:36:08 +0530

    Refactor findLonely method to use Counter and simplify logic

--- 2150.py
+++ 2150.py
@@ -1,23 +1,14 @@
 from typing import List
-import collections
+from collections import Counter
 
 
 class Solution:
     def findLonely(self, nums: List[int]) -> List[int]:
-        count = collections.defaultdict(int)
+        dic = Counter(nums)
+        ans = []
+
         for i in nums:
-            count[i] += 1
-        r = []
-        for i in nums:
-            if count[i] > 1:
-                continue
-            if i - 1 in count or i + 1 in count:
-                continue
+            if dic[i] == 1 and i - 1 not in dic and i + 1 not in dic:
+                ans.append(i)
 
-            r.append(i)
-
-        return r
-
-
-r = Solution().findLonely([10, 6, 5, 8])
-print(r)
+        return ans
