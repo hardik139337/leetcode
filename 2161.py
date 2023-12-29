93a0581c4740e604cc1fd57de25ecb4458c9f21c
hardik <hardik>
2023-12-29 10:44:29 +0530

    Refactor pivotArray method to correctly sort and place pivot element

--- 2161.py
+++ 2161.py
@@ -3,5 +3,20 @@ from typing import List
 
 class Solution:
     def pivotArray(self, nums: List[int], pivot: int) -> List[int]:
-        
-        return
+        ans = []
+
+        nums.remove(pivot)
+
+        i = 0
+        ans.append(pivot)
+
+        for j in nums:
+            if j < pivot:
+                ans.insert(i, j)
+                i = i + 1
+            elif j == pivot:
+                ans.insert(i + 1, j)
+            else:
+                ans.append(j)
+
+        return ans
